9b268198e0aa22a38821239bd14347d4897104f9
Roystan1999 <message>
2022-01-19 10:55:14 +0530

    agahj

---
+++ src/components/Sdata.jsx
@@ -0,0 +1,50 @@
+
+
+  export const details = [
+    {
+      name: "Roystan",
+      age: 23,
+     
+    },
+    {
+      name: "Abraham",
+      age: 27,
+     
+    },
+    {
+      name: "jack",
+      age: 22,
+     
+    },
+    {
+      name:"miz",
+      age:33,
+    
+    },
+    {
+      name:"john",
+      age:34,
+  
+    },
+    {
+      name:"yash",
+      age:45,
+  
+    },{
+      name:"michel",
+      age:52,
+    
+    },{
+      name:"reymestrio",
+      age:29,
+    
+    },{
+      name:"roman",
+      age:32,
+     
+    },{
+      name:"Mathew",
+      age:52,
+      
+    }
+  ]
